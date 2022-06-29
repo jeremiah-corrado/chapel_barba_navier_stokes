0424c9b91df02a0bde2f43139dfab8a117720a4c
Jeremiah Corrado <jeremiah.corrado@hpe.com>
2022-06-29 15:50:35 -0600

    added chpl step 6 implementation

--- chapel_ports/step_6.chpl
+++ chapel_ports/step_6.chpl
@@ -10,7 +10,7 @@ const dy = 2.0 / (ny - 1);
 config const sigma = 0.2;
 const dt = sigma * dx;
 
-writeln("Running 2D Linear Convection Simulation over: ");
+writeln("Running 2D Non-Linear Convection Simulation over: ");
 writeln();
 writeln("0 \t\t ", dx * (nx - 1));
 writeln("*-----(", nx, "x)-----* \t (dx = ", dx, ")");
@@ -27,25 +27,39 @@ writeln("with: c = ", c);
 
 // create an 2-dimensional array to represent the computational Domain
 var u : [{0..<nx, 0..<ny}] real;
+var v : [{0..<nx, 0..<ny}] real;
 
 // set up the initial conditions
 u = 1.0;
 u[(0.5 / dy):int..<(1.0 / dy + 1):int, (0.5 / dx):int..<(1.0 / dx + 1):int] = 2.0;
+v = 1.0;
+v[(0.5 / dy):int..<(1.0 / dy + 1):int, (0.5 / dx):int..<(1.0 / dx + 1):int] = 2.0;
 
 // apply the fd equation for nt iterations
 var un = u;
+var vn = v;
 for i in 0..#nt {
     u <=> un;
+    v <=> vn;
 
     foreach (i, j) in {1..<(nx-1), 1..<(ny-1)} {
-        u[i, j] = un[i, j] - (c * dt / dx * (un[i, j] - un[i, j - 1])) -
-                             (c * dt / dy * (un[i, j] - un[i - 1, j]));
+        u[i, j] = un[i, j] - (un[i, j] * c * dt / dx * (un[i, j] - un[i, j - 1])) -
+                             (vn[i, j] * c * dt / dy * (un[i, j] - un[i - 1, j]));
+
+        v[i, j] = vn[i, j] - (un[i, j] * c * dt / dx * (vn[i, j] - vn[i, j - 1])) -
+                             (vn[i, j] * c * dt / dy * (vn[i, j] - vn[i - 1, j]));
     }
 
     u[0, ..] = 1.0;
     u[.., 0] = 1.0;
     u[nx - 1, ..] = 1.0;
     u[.., ny - 1] = 1.0;
+
+    v[0, ..] = 1.0;
+    v[.., 0] = 1.0;
+    v[nx - 1, ..] = 1.0;
+    v[.., ny - 1] = 1.0;
 }
 
-write_array_to_file("./sim_output/step_5_output.txt", u);
+write_array_to_file("./sim_output/step_6_u_output.txt", u);
+write_array_to_file("./sim_output/step_6_v_output.txt", v);
---
+++ python_scripts/step_6.py
@@ -0,0 +1,70 @@
+import numpy
+import sympy
+from matplotlib import pyplot
+from matplotlib import cm
+import sys
+
+show_plots = "--show_plots" in sys.argv[1:]
+
+### setup simulation parameters
+nx = 81
+ny = 81
+nt = 100
+c = 1
+dx = 2 / (nx - 1)
+dy = 2 / (ny - 1)
+sigma = .2
+dt = sigma * dx
+
+### setup computational domain
+x = numpy.linspace(0, 2, nx)
+y = numpy.linspace(0, 2, ny)
+
+u = numpy.ones((ny, nx))
+v = numpy.ones((ny, nx));
+
+un = numpy.ones((ny, nx))
+vn = numpy.ones((ny, nx))
+
+### setup initial conditions
+u[int(.5 / dy):int(1 / dy + 1),int(.5 / dx):int(1 / dx + 1)] = 2
+v[int(.5 / dy):int(1 / dy + 1),int(.5 / dx):int(1 / dx + 1)] = 2
+
+
+fig, ax = pyplot.subplots(subplot_kw={"projection": "3d"})
+X, Y = numpy.meshgrid(x, y)
+surf = ax.plot_surface(X, Y, u[:], cmap=cm.viridis)
+if show_plots: pyplot.show()
+
+### apply fd equation for nt iterations
+for n in range(nt):
+    un = u.copy()
+    vn = v.copy()
+
+    u[1:, 1:] = (un[1:, 1:] -
+                 (un[1:, 1:] * c * dt / dx * (un[1:, 1:] - un[1:, :-1])) -
+                  vn[1:, 1:] * c * dt / dy * (un[1:, 1:] - un[:-1, 1:]))
+    v[1:, 1:] = (vn[1:, 1:] -
+                 (un[1:, 1:] * c * dt / dx * (vn[1:, 1:] - vn[1:, :-1])) -
+                 vn[1:, 1:] * c * dt / dy * (vn[1:, 1:] - vn[:-1, 1:]))
+
+    u[0, :] = 1
+    u[-1, :] = 1
+    u[:, 0] = 1
+    u[:, -1] = 1
+
+    v[0, :] = 1
+    v[-1, :] = 1
+    v[:, 0] = 1
+    v[:, -1] = 1
+
+fig, ax = pyplot.subplots(1, 2, subplot_kw={"projection": "3d"})
+X, Y = numpy.meshgrid(x, y)
+surf = ax[0].plot_surface(X, Y, u[:], cmap=cm.viridis)
+ax[0].set_title("u(x, y)")
+surf = ax[1].plot_surface(X, Y, v[:], cmap=cm.viridis)
+ax[1].set_title("v(x, y)")
+if show_plots: pyplot.show()
+
+numpy.savetxt("./sim_output/step_6_py_u_output.txt", u, fmt='%.8f')
+numpy.savetxt("./sim_output/step_6_py_v_output.txt", v, fmt='%.8f')
